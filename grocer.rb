b99f5210ff28c7005cf2c9a2ae8cfd15ac5a4aa9
James Inglis <jinglis12@googlemail.com>
2019-12-03 20:23:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   i = 0
   item = nil
-  while i < collection.length
+  while i < collection.size
     if collection[i][:item] == name
       item = collection[i]
     end
