4175696cf1fd59da6ecbf876052a9f19e679c62e
James Inglis <jinglis12@googlemail.com>
2019-12-03 18:04:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  
+  print cart
   i = 0
   cart_array = []
   while i < cart.length
@@ -29,7 +29,7 @@ def consolidate_cart(cart)
     else
       cart_item_i = 0
       while cart_item_i < cart_array.length
-        if cart_array[cart_item_i][:item] === cart[i][:item]
+        if cart_array[cart_item_i][:item] == cart[i][:item]
           cart_array[cart_item_i][:count] += 1
         end
       cart_item_i += 1
@@ -37,6 +37,7 @@ def consolidate_cart(cart)
     end
     i += 1
   end
+  print cart_array
   cart_array
 end
 
